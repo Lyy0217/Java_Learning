4635aef70f896c9a02d5fa183adade993dc15b81
lyy <1126137994@qq.com>
2019-05-22 23:30:40 +0800

    add PrimaryType.java

---
+++ code007/src/src/PrimaryTypes.java
@@ -0,0 +1,27 @@
+public class PrimaryTypes {
+    public static void main(String[] args){
+        byte byteVar = 100;
+        System.out.println(byteVar);
+
+        short shortVar = 30000;//32768
+        System.out.println(shortVar);
+
+        int intVar = 100000000;
+        System.out.println(intVar);
+
+        long longVar = 800000000L;
+        System.out.println(longVar);
+
+        float floatVar = 100.00000066f;
+        System.out.println(floatVar);
+
+        double doubleVar = 100.00000066;
+        System.out.println(doubleVar);
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+
+        char charVar = 'a';
+        System.out.println(charVar);
+    }
+}
