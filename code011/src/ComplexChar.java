d0a76be5c78429a07d7aff4b889b594a5b17304a
lyy <1126137994@qq.com>
2019-05-24 20:27:18 +0800

    add src

---
+++ code011/src/ComplexChar.java
@@ -0,0 +1,11 @@
+public class ComplexChar {
+    public static void main(String[] args) {
+        int a = 65;
+        char ch = (char)a;
+        char zang = '\u81e7';
+
+        System.out.println(ch);
+
+        System.out.println(zang);
+    }
+}
