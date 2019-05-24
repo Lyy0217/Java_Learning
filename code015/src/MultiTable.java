585b20c4707a59b267376a2646566e8c730ed6fe
lyy <1126137994@qq.com>
2019-05-25 01:24:38 +0800

    add src

--- code015/src/MultiTable.java
+++ code015/src/MultiTable.java
@@ -1,10 +1,10 @@
 public class MultiTable {
     public static void main(String[] args) {
-        for(int i=1;i<=9;i++){
+        for (int i = 1; i <= 9; i++) {
             String line = "";
-            for(int j=1;j<=9;j++){
-                if(i<j)break;
-                line = line+ j+"*"+i+"="+(i*j)+"\t";
+            for (int j = 1; j <= 9; j++) {
+                if (i < j) break;
+                line = line + j + "*" + i + "=" + (i * j) + "\t";
                 //System.out.println(line);
             }
             System.out.println(line);
