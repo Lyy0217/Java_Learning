d7dde2dfa4137dec82bab9850bd7c1176dab43f4
lyy <1126137994@qq.com>
2019-06-05 14:51:55 +0800

    add 019 020

---
+++ code019/src/CreatArray.java
@@ -0,0 +1,9 @@
+public class CreatArray {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        System.out.println(intArray[9]);
+
+        double[] doubleArray = new double[20];
+        System.out.println(doubleArray[19]);
+    }
+}
