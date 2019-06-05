bfbb7a64d90a9089b7129380c681ea9042f72cd2
lyy <1126137994@qq.com>
2019-06-06 01:30:28 +0800

    0707_类的使用

---
+++ 02_Java的类/07_类的使用/src/MainBoard.java
@@ -0,0 +1,9 @@
+public class MainBoard {
+    CPU cpu;
+    Memory memory;
+    Storage storage;
+    String modol;
+
+    //上市年份
+    int year;
+}
---
+++ 02_Java的类/07_类的使用/src/Phone.java
@@ -0,0 +1,8 @@
+public class Phone {
+    Screen screen;
+    MainBoard mainBoard;
+    double price;
+    boolean hasFigurePrintlocker;
+
+    Phone prePhone;
+}
